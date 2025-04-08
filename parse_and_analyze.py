97ef1261b35d4d9ad896b845983023d1a8827f41
Daria <dasha.582007@gmail.com>
2025-04-08 19:44:49 +0300

    the second part of ML is ready

--- parse_and_analyze.py
+++ parse_and_analyze.py
@@ -1,15 +1,16 @@
 import json
 from pathlib import Path
 from analyze_dialogue import analyze_dialogue
+import pandas as pd
 
-
+df = pd.read_excel('data.xlsx')
 def parse_first_dialogue_and_analyze(file_path: str):
     # Загрузка данных
     with open(file_path, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
     # Извлекаем первый диалог
-    first_dialog = data["dialogs"][0]
+    first_dialog = data
     lines = first_dialog["lines"]
 
     # Формируем текст диалога в виде: Менеджер: ... Клиент: ...
@@ -18,14 +19,15 @@ def parse_first_dialogue_and_analyze(file_path: str):
     )
 
     print("=== Отправляем следующий диалог в LLM ===")
-    print(dialogue_text[:1000])  # усеченный вывод
-
     # Отправляем в LLM для анализа
     analysis_result = analyze_dialogue(dialogue_text)
 
     if analysis_result:
         print("=== Результат анализа ===")
         print(analysis_result.model_dump_json(indent=2))
+        df.loc[len(df)] = {'date': None, 'manager_id': None, 'manager_score': response.manager_performance.score, 'manager_explanation': response.manager_performance.explanation,
+                   'client_emotion': response.client_emotion.score, 'client_explanation': response.client_emotion.explanation}
+        df.to_excel('data.xlsx', index=False)
         return analysis_result
     else:
         print("Анализ не удался или модель отказалась отвечать.")
@@ -33,5 +35,5 @@ def parse_first_dialogue_and_analyze(file_path: str):
 
 
 # Пример вызова
-if __name__ == "__main__":
-    parse_first_dialogue_and_analyze("dialog.json")
+response = parse_first_dialogue_and_analyze("dialog.json")
+df
