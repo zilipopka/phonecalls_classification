679e8e57cbe7451e96d9bd19ace5c21a63ee96dc
reskeee <comandos200809@gmail.com@gmail.com>
2025-04-20 12:27:56 +0700

    Spelling corection

--- prepare.py
+++ prepare.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 from dotenv import load_dotenv
-from schemas import Responce
+from schemas import Response
 import os
 
 load_dotenv()
@@ -40,7 +40,7 @@ def preprocessing(text):
                 "content": [{"type": "text", "text": text}]
             }
         ],
-        response_format=Responce
+        response_format=Response
     )
 
     response = completion.choices[0].message.parsed
